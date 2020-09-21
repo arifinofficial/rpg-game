23cd84efaed84b352058184425e65098a6e9448b
arifinofficial <arifinofficial30@gmail.com>
2020-09-22 00:49:16 +0800

    Create character model

---
+++ Rpg-Game/Models/Character.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Rpg_Game.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "Frodo";
+        public int HitPoints { get; set; } = 100;
+        public int Strength { get; set; } = 10;
+        public int Defense { get; set; } = 10;
+        public int Intellegence { get; set; } = 10;
+        public RpgClass Class { get; set; } = RpgClass.Knight;
+    }
+}
